4dc6a119037398aceedea483e5cd6f41ce94e7d6
gittest <384813636@qq.com>
2018-03-29 11:26:58 +0800

    branch

---
+++ tests/IndexTest.php
@@ -0,0 +1,14 @@
+<?php
+use tests\TestCase;
+class IndexTest extends TestCase
+{
+
+    public function testSomethingIsTrue()
+    {
+         $this->assertEquals(
+            'Hello Think',
+            'Hello Think'
+        );
+    }
+
+}
